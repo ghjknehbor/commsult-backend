acede7ca6a6ba6a0539f76d5408c63c0a7548d1f
ghjknehbor <150774225+ghjknehbor@users.noreply.github.com>
2025-05-08 18:35:43 +0700

    repo refactor to use promises

--- repository/app_repo.js
+++ repository/app_repo.js
@@ -2,80 +2,84 @@ const { Movie, Rating } = require('../models/app_models')
 const mongoose = require('mongoose')
 
 
-function findMovies() {
-    var movies = Movie.find()
-    if (!movies) {
-        return { message: "no movies" }
-    } else {
+async function findMovies() {
+    try {
+        const movies = await Movie.find()
         return movies
+    } catch (error) {
+        console.error(`Error getting movies: ${error}`)
+        return error
     }
 }
 
-function findMovie(id) {
+async function findMovie(id) {
     if (!id) {
         return { message: "no id provided" }
     }
-    var movies = Movie.findById(id)
-    if (!movies) {
-        return { message: "no movies" }
-    } else {
+    try {
+        const movies = await Movie.findById(id)
         return movies
+    } catch (error) {
+        console.error(`Error getting movie: ${error}`)
+        return error
     }
 }
 
-function addMovie(newMovie) {
+async function addMovie(newMovie) {
     if (!newMovie) {
         return { message: "no movie or malformed request" }
     }
-    var movies = new Movie(newMovie)
-    movies.save().then(savedMovie => {
-        return savedMovie
-    }).catch(error => {
-        return { message: error }
-    })
+
+    try {
+        var movies = new Movie(newMovie)
+        await movies.save()
+    } catch (error) {
+        console.error(`Error adding movie: ${error}`)
+        return error
+    }
 }
 
-function delMovie(id) {
+async function delMovie(id) {
     if (!id) {
         return { message: "no id provided" }
     }
-    Movie.deleteOne(id)
-    .then(() => {
-        return 0
-    })
-    .catch((error) => {
+
+    try {
+        const success = await Movie.deleteOne(id)
+        return success
+    } catch (error) {
+        console.error(`Error deleting movie: ${error}`)
         return error
-    })
+    }
 }
 
-function findRatings(data) {
+async function findRatings(data) {
     var movie = mongoose.Types.ObjectId.createFromHexString(data)
-    Rating.find(movie)
-    .then(ratings => {
-        return ratings
-    })
-    .catch(error => {
+    try {
+        const results = await Rating.find(movie)
+        return results
+    } catch (error) {
         return error
-    })
+    }
 }
 
-function createRating(rate) {
+async function addRating(rate) {
     var newRating = new Rating(rate)
-    newRating.save().then(saved => {
-        return saved
-    }).catch(error => {
-        return error
-    })
+    try {
+        const results = await newRating.save()
+        return results
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-function deleteRating(id) {
-    Rating.findByIdAndDelete(id)
-    .then(() => {
+async function deleteRating(id) {
+    try {
+        await Rating.findByIdAndDelete(id)
         return 0
-    })
-    .catch((error) => {
+    } catch (error) {
         return error
-    })
+    }
 }
 
-module.exports = { findMovies, findMovie, addMovie, delMovie, findRatings, createRating, deleteRating }
+module.exports = { findMovies, findMovie, addMovie, delMovie, findRatings, addRating, deleteRating }
